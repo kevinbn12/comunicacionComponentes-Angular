34138a22f977b0561fe14bbaf198ec9de08595c1
sena <sena@deskop-cqopt7u>
2025-11-19 11:48:02 -0500

    componente 1 - navbar

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Component1 } from './component1/component1';
+import { Component2 } from './component2/component2';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Component1, Component2],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
--- src/app/component1/component1.ts
+++ src/app/component1/component1.ts
@@ -1,8 +1,9 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-component1',
-  imports: [],
+  imports: [RouterLinkActive,RouterLink],
   templateUrl: './component1.html',
   styleUrl: './component1.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
